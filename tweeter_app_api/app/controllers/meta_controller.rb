2fc241e5a8bbdd31575e6a058d77ca4496ac485e
theaharris <supershay84@gmail.com>
2021-01-16 14:11:52 -0500

    Commit 12: Added validations

---
+++ tweeter_app_api/app/controllers/meta_controller.rb
@@ -0,0 +1,5 @@
+class MetaController < ApplicationController
+
+def about
+    render json: { author: "President Kool-chair", last_updated: "10 Jan 2017" }
+end
--- tweeter_app_api/app/models/tweet.rb
+++ tweeter_app_api/app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+    validates :title, :content, :author, presence: true
+    validates :content, uniqueness: true
 end
